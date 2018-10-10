7686fa24270255ab3441d005a1babd1eaa11f4b8
ScottStrasser <43963788+ScottStrasser@users.noreply.github.com>
2018-10-10 11:33:27 -0500

    Add files via upload

---
+++ const.php
@@ -0,0 +1,12 @@
+<?php
+
+define("MUSER","strasser_s_admin");
+define("MPASS","fiz76jym");
+define("MSERVER","cnmtsrv1");
+define("MDB","strasser_s");
+
+if (basename($_SERVER['PHP_SELF']) == "const.php") {
+  die(header("HTTP/1.0 404 Not Found"));
+}
+
+?>
