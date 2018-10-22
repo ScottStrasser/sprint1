e09838e4497a61f8b725e0bb0e638caf80b5b8f7
Jack Allaire <40919354+jra10542@users.noreply.github.com>
2018-10-22 11:02:17 -0500

    Add files via upload

---
+++ const.php
@@ -0,0 +1,9 @@
+<?php
+define("MUSER","allaire_j_admin");
+define("MPASS","ffun30laz");
+define("MSERVER","cnmtsrv1");
+define("MDB","allaire_j");
+if (basename($_SERVER['PHP_SELF']) == "const.php") {
+  die(header("HTTP/1.0 404 Not Found"));
+}
+?>
